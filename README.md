2b92554468becd7e9c2e8386a5314ca9c7221f1a
Balaji Radhakrishnan <rbalajirrk@gmail.comm>
2025-01-03 20:51:53 -0500

    Lots of new features and bug fixes made.
    New Features: Changed controls to be more intuitive, restructured files to be maven standard compliant and added pom.xml for Maven java project building, updated readme, added ability to adjust fps, added cycling between algorithms.
    
    Bug Fixes: Refactored code implementation to follow a more individual component structure for composability and code maintainability and fixed algorithm continuing to keep sorting even after lines were already sorted.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Sorting-Algorithm-Visualizer
-A program that helps the user visualize the time complexity differences between sorting algorithms.
---
+++ sorting-algorithms-visualizer/src/main/java/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+
+public class Main { 
+
+	public static void main(String args[]) {
+		DrawingSurface drawing = new DrawingSurface();
+		PApplet.runSketch(new String[]{""}, drawing);
+		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
+
+//		window.setSize(406, 329); // actual screen size (400, 300)
+		window.setMinimumSize(new Dimension(406, 329));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+
+		window.setVisible(true);
+		
+	}
+}
